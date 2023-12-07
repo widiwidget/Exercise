93f75b464d5f78403af3e738c4388d3814af8396
codriot <mustafalpc0@gmail.com>
2023-12-07 13:30:21 +0300

    proje kaba taslak bitti

---
+++ lib/core/const/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+enum AppColor {
+  royalWedding,
+  lakeSide,
+  camellia,
+  whiteheaven,
+  snowflake,
+  tahin
+}
+
+extension AppColorExtension on AppColor {
+  Color getColor() {
+    switch (this) {
+      case AppColor.royalWedding:
+        return const Color(0xfffce4e2);
+      case AppColor.lakeSide:
+        return const Color(0xffd6f0ef);
+      case AppColor.camellia:
+        return const Color(0xfff46956);
+      case AppColor.whiteheaven:
+        return const Color(0xfffefefe);
+      case AppColor.snowflake:
+        return Colors.grey[200]!;
+      case AppColor.tahin:
+        return const Color(0xfffefaf7);
+    }
+  }
+}
